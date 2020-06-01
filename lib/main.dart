e475ac3ac0e2476275ed309473c992ca92507e27
Shesman <shesmanrenatoitz@gmail.com>
2020-06-01 10:17:14 -0300

    Add App no GitHub

---
+++ lib/main.dart
@@ -0,0 +1,108 @@
+import 'models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  final _transaction = [
+    Transaction (
+      id: 't1',
+      title: 'Novo Tênias de Corrida',
+      value: 439.90,
+      date: DateTime.now(),
+    ),
+
+    Transaction (
+      id: 't2',
+      title: 'Conta de Energia',
+      value: 360.80,
+      date: DateTime.now(),
+    )
+
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais')
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+         Container(
+          child: Card(
+             child: Text('Gráfico'),
+             elevation: 5,
+           ),
+         ),
+         Column(
+           children: _transaction.map((tr) {
+             return Card(
+               child: Row(
+                children: <Widget>[
+
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 10
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      )
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'R\$ ${tr.value.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.purple,
+
+                      ),
+                    ),
+                  ),
+
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(tr.title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        DateFormat('d MMM y').format(tr.date),
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                    ],
+                  ),
+
+                 ],
+               ),
+             );
+           }).toList(),
+         ),
+       ],
+      ),
+    );
+  }
+}
